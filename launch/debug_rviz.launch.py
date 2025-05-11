813bda30b1638d5262a1c29f5bd502dc428f3d00
jimin3722 <jimin372215@gmail.com>
2025-05-11 21:57:02 +0900

    Debug : debug rviz

--- launch/debug_rviz.launch.py
+++ launch/debug_rviz.launch.py
@@ -4,25 +4,24 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from pathlib import Path
 
 
 def generate_launch_description():
 
-    local_rviz = os.path.join(
-        os.getenv('PWD', ''),    # 워크스페이스 루트 경로
-        'src',
-        'emcl2_ros2',            # 실제 패키지 폴더 이름
-        'rviz',
-        'local.rviz'
-    )
+    # 이 런치 파일이 src/emcl2_ros2/launch 디렉터리에 있다고 가정
+    # Path(__file__) → .../src/emcl2_ros2/launch/your_launch.py
+    pkg_root = Path(__file__).resolve().parents[1]
+    # → .../src/emcl2_ros2
 
-    # 커스텀 RViz 노드
+    local_rviz = pkg_root / 'rviz' / 'local.rviz'
+
     custom_rviz_node = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2_local',
         output='screen',
-        arguments=['-d', local_rviz]
+        arguments=['-d', str(local_rviz)],
     )
 
     return LaunchDescription([
